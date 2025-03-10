a59939c2334c66b660dab8ffa9a8ee1aa8382019
Igor2 <sysroot314@gmail.com>
2025-03-10 21:46:14 +0300

    Add PWA support with service worker, meta tags, and icons

---
+++ sw.js
@@ -0,0 +1,76 @@
+const CACHE_NAME = 'courier-app-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    '/manifest.json',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png',
+    'https://fonts.gstatic.com/s/materialicons/v140/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2'
+];
+
+// Установка service worker и кэширование файлов
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Кэширование ресурсов');
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+// Активация service worker и удаление старых кэшей
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Удаление старого кэша:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Перехват запросов и обработка кэшированных ресурсов
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Возвращаем кэшированный ответ, если он есть
+                if (response) {
+                    return response;
+                }
+
+                // Если ответа нет в кэше, делаем запрос к сети
+                return fetch(event.request).then(
+                    (networkResponse) => {
+                        // Проверяем валидность ответа
+                        if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+                            return networkResponse;
+                        }
+
+                        // Кэшируем новый ответ
+                        const responseToCache = networkResponse.clone();
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return networkResponse;
+                    }
+                );
+            })
+    );
+}); 
